a6eecc92ec860b92e555a6cf7cf35aa2f8e3a487
mehmetvural <mehmetvurall@gmail.com>
2023-03-02 23:08:55 -0500

    I added some more classes on Thursday in week4

--- src/week4/ArraysHomework4_6.java
+++ src/week4/ArraysHomework4_6.java
@@ -4,7 +4,7 @@ import java.util.Locale;
 
 public class ArraysHomework4_6 {
     public static void main(String[] args) {
-        //4. Writ numberArre a Java program to test if an array contains a specific value. For example an int array contains 99 or not?
+        //4. Write a Java program to test if an array contains a specific value. For example an int array contains 99 or not?
         int numberArr[] = {344,555,66,88,999,66,55};
         int number = 88;
         System.out.println(contains(numberArr,number));
@@ -21,6 +21,10 @@ public class ArraysHomework4_6 {
         if(indexNumber != -1) System.out.println("carElement's index number is " + indexNumber);
         else System.out.println("There is no such an element in arrayCar.");
 
+
+        //Question 6 :
+        removeElement(arrayCar,"KIA");
+        removeElement2(arrayCar, "TOYOTA");
 
     }
 
@@ -42,5 +46,38 @@ public class ArraysHomework4_6 {
             }
 
         return result;
+        }
+
+//        6. Write a Java program to remove a specific element from an array.
+        public static void removeElement(String arr[], String removeElement){
+        int counter = 0;
+        for(String str : arr){
+            if(str.equals(removeElement)){
+                //Solution 1:
+                int indexNum = findIndexOfElement(arr,removeElement);
+                arr[indexNum] = "free-2";
+                System.out.println(arr[indexNum] + " -->index number : " + indexNum);
+
+                //Solution 2 :
+                arr[counter] = "free-1";
+                System.out.println(arr[counter] + " -->index number : " + counter);
+
+
+            }
+        counter++;
+        }
+
+        }
+
+            //Solution 3 : arr [] = {"BMW", "HONDA", "TOYOTA", "KIA", "HYUNDAI", "AUDI"};
+        public static void removeElement2(String arr[], String removeElement){
+            for(int i = 0; i<arr.length; i++){
+                if(arr[i].equals(removeElement)) {
+                    arr[i] = "EMPTY";
+                System.out.println(arr[i] + " -->index number : " + i);
+                break; // exit from for loop immediately.
+                }else if(i == arr.length-1) System.out.println("There is no such an element in the array.");
+
+            }
         }
 }
---
+++ src/week4/StringMethods.java
@@ -0,0 +1,97 @@
+package week4;
+
+import java.util.Arrays;
+
+public class StringMethods {
+    public static void main(String[] args) {
+        String str1 = "Java";
+        String str2 = "   Python";
+        String str3 = "Java Script.";
+
+        // length() method : Length of String > .length()
+        int stringLength = str1.length();
+        System.out.println("str1 length is : " + stringLength);
+
+        // equals() method :
+        boolean equalOrNOt = str1.equals("Java");
+        System.out.println(equalOrNOt);
+
+        //charAt() method :
+        char charAtReturn = str2.charAt(3);
+        System.out.println(charAtReturn);
+
+        // concat() method :
+        String newStr2 = str2.concat(" Programming Language.              ");
+        System.out.println(newStr2);
+        System.out.println(str1.concat(" Programming Language."));
+        System.out.println(newStr2.equals(str2.concat(" Programming Language.")));
+
+        //indexOf() method
+        System.out.println(str3.indexOf("S"));
+
+        // replace() method
+         System.out.println(str3.replace('a','i'));
+
+         //startsWith() method
+        System.out.println(str2.startsWith("Py"));
+
+        //endsWith() method
+        System.out.println(str3.endsWith("?"));
+
+        //substring(int beginIndex) method
+        System.out.println(str3.substring(7));
+
+        //substring(int beginIndex, int endIndex) method
+        System.out.println(newStr2.substring(7, 18));
+
+        // toCharArray()
+        System.out.println(Arrays.toString(str1.toCharArray()));
+
+        //trim() method  : removing the spaces front and end of the string.
+        System.out.println(newStr2.trim());
+
+        // compareTo() method
+        System.out.println(str1.compareTo(str2));
+
+        //ESCAPE characters in String
+        String str4 = "This is the \"String\" class.\n 'A' ";
+        System.out.println(str4);
+
+        // Creating the strings using the new keywords :
+        String name = new String("Java String");
+        System.out.println(name);
+
+        // append() method
+        StringBuffer sb = new StringBuffer("Hello ");
+        sb.append("Java"); // now original string is changed
+        System.out.println(sb);
+
+        //insert() method
+        StringBuffer sb1 = new StringBuffer("Hello ");
+        sb1.insert(2, "Java"); // Now original string is changed
+        System.out.println(sb1);
+
+        // replace() method : The replace() method replaces the given string from the specified beginIndex and endIndex-1.
+        StringBuffer sb2=new StringBuffer("Hello");
+        sb2.replace(1,3,"Java");
+        System.out.println(sb2);
+
+        // delete() method : The delete() method of StringBuffer class deletes the string from the specified beginIndex to endIndex-1.
+        StringBuffer sb3=new StringBuffer("Hello");
+        sb3.delete(1,3);
+        System.out.println(sb3);
+
+        // reverse() method : The reverse() method of StringBuilder class reverses the current string
+        StringBuffer sb4 = new StringBuffer("Hello");
+        sb4.reverse();
+        System.out.println(sb4);
+
+
+
+
+
+
+
+
+    }
+}
